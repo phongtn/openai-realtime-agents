58de39072089951c81b3780167f1486c8060409e
PhongTn <phongtn.group1@gmail.com>
2026-01-10 16:23:05 +0700

    update `.gitignore` and dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./lib/envSetup";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Realtime API Agents",
